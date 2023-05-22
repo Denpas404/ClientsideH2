202740bf8c0497f45224a29b83086cbdfea3b731
Denpas404 <102860689+Denpas404@users.noreply.github.com>
2023-05-22 20:06:22 +0200

    TicTacToe done

--- Mandag22/Project1/js/script.js
+++ Mandag22/Project1/js/script.js
@@ -8,39 +8,52 @@ setInterval(clockTime, 1000);
 
 const gameBoard = document.querySelector(".gameboard");
 
+let playerXTurn = true; // Angiver om det er spiller X's tur
+let currentPlayer = "X"; // Den aktuelle spiller
+let playerOne = document.getElementById("playerOne"); // Spiller 1 element
+let playerTwo = document.getElementById("playerTwo"); // Spiller 2 element
+let playerOneScore = 0; // Spiller 1 score
+let playerTwoScore = 0; // Spiller 2 score
+let moves = 0; // Tæller for antal træk
 
-let playerXTurn = true
-let playerOne = document.getElementById("playerOne");
-let playerTwo = document.getElementById("playerTwo");
-let playerOneScore = 0;
-let playerTwoScore = 0;
 
-const createEvt = (fields) => {
-    fields.forEach((btn) => {
-        btn.addEventListener("click", (e) => {
-            if (e.target.innerHTML == "") {
-                e.target.innerHTML = playerXTurn ? "X" : "O";
+const createEvt = (fields) => {    
+    fields.forEach((btn) => {     // Itererer gennem hvert felt
+        btn.addEventListener("click", (e) => { // "Hændelseslytter" for klik begivenheden på en knap
+
+            if (e.target.innerHTML === "") {
+                const currentPlayer = playerXTurn ? "X" : "O";
+                e.target.innerHTML = currentPlayer;
+                
+                moves++; // Øger antallet af træk med 1
+
                 setTimeout(() => {
-                if (isWinner()){
-                     alert("Vinderen er: " + (playerXTurn ? "X" : "O"));
-                        if (playerXTurn) {
+                    if (isWinner()) {
+                        alert("Vinderen er: " + currentPlayer);
+                        if (currentPlayer === "X") {
                             playerOneScore++;
                             playerOne.innerHTML = playerOneScore;
-                        } 
-                        else {
+                        } else {
                             playerTwoScore++;
-                            playerTwo.innerHTML = playerTwoScore;                            
+                            playerTwo.innerHTML = playerTwoScore;
                         }
-                       return; 
-                }}, 100); 
+                        
+                        return;                        
+                    }
+                    else if (moves === 9) {
+                        alert("Uafgjort");
+                        return;
+                    }
+                }, 100);
                 
-                playerXTurn = !playerXTurn;
+                playerXTurn = !playerXTurn; // Skifter til den næste spillers tur
             }
         });
     });
 };
-const isWinner = () => {
 
+const isWinner = () => {
+    // Tjekker hver række for en vinder
     for (let row = 0; row < 3; row++) {
         if (document.querySelector(`#r${row}c0`).innerHTML ===
             document.querySelector(`#r${row}c1`).innerHTML &&
@@ -49,6 +62,7 @@ const isWinner = () => {
             document.querySelector(`#r${row}c0`).innerHTML !== "")
             return true;
     }
+    // Tjekker hver kolonne for en vinder
     for (let col = 0; col < 3; col++) {
         if (document.querySelector(`#r0c${col}`).innerHTML ===
             document.querySelector(`#r1c${col}`).innerHTML &&
@@ -57,23 +71,27 @@ const isWinner = () => {
             document.querySelector(`#r0c${col}`).innerHTML !== "")
             return true;
     }
-
+    // Tjekker diagonalen fra øverste venstre hjørne til nederste højre hjørne
     if (document.querySelector("#r0c0").innerHTML ===
         document.querySelector("#r1c1").innerHTML &&
         document.querySelector("#r0c0").innerHTML ===
         document.querySelector("#r2c2").innerHTML &&
         document.querySelector("#r0c0").innerHTML !== "")
         return true;
-
+    // Tjekker diagonalen fra øverste højre hjørne til nederste venstre hjørne
     if (document.querySelector("#r0c2").innerHTML ===
         document.querySelector("#r1c1").innerHTML &&
         document.querySelector("#r0c2").innerHTML ===
         document.querySelector("#r2c0").innerHTML &&
         document.querySelector("#r0c2").innerHTML !== "")
         return true;
+    
+    // Returnerer false hvis der ikke er en vinder
+    return false;
 };
 
 const init = () => {
+    // Genererer knapperne til spillepladen
     for (let row = 0; row < 3; row++) {
         for (let col = 0; col < 3; col++) {
             let btn = document.createElement("button");
@@ -82,41 +100,25 @@ const init = () => {
             gameBoard.appendChild(btn);
         }
     }
+    // Henter alle felterne (knapperne) på spillepladen
     const fields = document.querySelectorAll(".field");
+    // Opretter hændelseslyttere for knapperne
     createEvt(fields);
 };
 
+// Initialiserer spillet ved at kalde init-funktionen
 init();
 
 const resetbtn = document.getElementById("reset");
 resetbtn.addEventListener("click", () => {
-  const fields = document.querySelectorAll(".field");
-  fields.forEach((btn) => {
-    btn.innerHTML = ""; // Clear the content of each button
-  });
-  playerXTurn = true; // Reset the player turn to X
+    const fields = document.querySelectorAll(".field");
+    fields.forEach((btn) => {
+        btn.innerHTML = ""; // Rydder indholdet af hvert felt
+    });
+    playerXTurn = true; // Nulstiller spillerens tur til X
+    moves = 0; // Nulstiller antallet af træk til 0
 });
 
 
 
-// fetch('https://api.publicapis.org/entries')
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error('Network response was not OK');
-//         }
-//         return response.json();
-//     })
-//     .then(data => {
-//         // Handle the data returned from the API
-//         const apiList = document.getElementById('apiList');
-//         for (let i = 0; i < data.entries.length; i++) {
-//             const listItem = document.createElement("li");
-//             listItem.textContent = data.entries[i].API;
-//             apiList.appendChild(listItem);
-//         }
 
-//     })
-//     .catch(error => {
-//         // Handle any errors that occurred during the request
-//         console.error('Error:', error);
-//     });
