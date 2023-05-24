bb56df821ca623a70c5834b79cd53e73a69798f1
Dennis Paaske <102860689+Denpas404@users.noreply.github.com>
2023-05-24 13:43:25 +0200

    navbar swapi

--- Onsdag24/Project2/js/script.js
+++ Onsdag24/Project2/js/script.js
@@ -1,12 +1,55 @@
 const baseUrl = "https://swapi.dev/api/";
 
-async function getData() {
-   try {
-    const response = await fetch(baseUrl);
-    return await response.json();    
-   } catch (error) {
-         console.log(error);
-   }
+const menuBar = document.getElementById("navbar");
 
+fetch(baseUrl)
+  .then((response) => {
+    return response.json();
+  })
+  .then((menuData) => {
+    for (let menuItem in menuData) {
+      let navItem = document.createElement("a");
+      navItem.addEventListener("click", navClick);
+      navItem.className = "nav-bar";
+      navItem.innerText = menuItem;
+      navItem.href = menuData[menuItem];
+      menuBar.appendChild(navItem);
+    }
+    /*  Old code
+        for (let menuItem in menuData) {
+            let li = document.createElement("li");
+            li.innerHTML = `<a href="#">${menuItem}</a>`;
+            menuBar.appendChild(li);
+        }
+        */
+  });
+
+async function navClick(e) {
+  e.preventDefault();
+  const data = await getData(this.href);
+  console.log(data);
+  // alert(this.href)
+  const main = document.getElementById("main");
+
+  for (let item in data.results) {
+    let attributes = data.results[item];
+    for (let attr in attributes) {
+      let p = document.createElement("p");
+      let strong = document.createElement("strong");
+      strong.innerText = attr;
+      p.appendChild(strong);
+      p.innerHTML += `: ${attributes[attr]}`;
+      main.appendChild(p);
+    }
+    main.appendChild(document.createElement("hr"));
+  }
 }
-getData();
+
+async function getData(Url) {
+  try {
+    const response = await fetch(Url);
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
